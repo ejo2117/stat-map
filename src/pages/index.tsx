25dca916091dfb669427011bf1977a555281676e
Ethan <ejo2117@columbia.edu>
2024-08-16 20:40:10 -0400

    feat: add basic example from docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
+import MapChart from "@/components/map/MapChart";
+
 export default function Home() {
 	return (
-		<h1>Hello World!</h1>
+		<>
+			<h1>Hello World!</h1>
+			<MapChart />
+		</>
 	);
 }
