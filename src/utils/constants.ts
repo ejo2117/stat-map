473ed67947a72a57e652291882315427204529f5
Ethan <ejo2117@columbia.edu>
2024-08-18 22:04:20 -0400

    chore: remove countries that don't need translation from constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,16 +2,10 @@ export const ONE_SECOND = 1000;
 
 export const ALTERNATE_COUNTRY_NAMES = {
 	'Congo': 'Republic of the Congo',
-	// 'French Guiana': '', // TODO: not a simple translation error here.
 	'French Southern Territories': 'French Southern and Antarctic Lands',
 	'Georgia': 'Georgia_(country)',
 	'Ireland': 'Republic of Ireland',
-	// 'Monaco': '', // TODO: breaks on missing "HDI" field
-	// 'New Caledonia': '', // TODO: breaks on missing "HDI" field
-	// 'North Korea': '', // TODO: breaks on missing "HDI" field
 	'Timor': 'East Timor',
-	// 'West Bank': '', // TODO: breaks on missing "HDI", "GDP" fields
-	// 'Western Sahara': '', // TODO: breaks on missing "HDI", "GDP" fields
 }
 
 export const COUNTRIES_WITH_ALTERNATE_NAME = Object.keys(ALTERNATE_COUNTRY_NAMES)
